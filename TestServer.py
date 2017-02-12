b63ba726b0dce09242c102719f16e931edb73cff
Carter Slocum <carter440@gmail.com>
2017-02-12 11:09:52 -0800

    port 80

--- TestServer.py
+++ TestServer.py
@@ -20,4 +20,4 @@ def work_page():
 	return render_template('CarterProjects.html', posts = projects.makePosts("ProjectPosts.txt"))
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')	
+	app.run(host='0.0.0.0', port=80)	

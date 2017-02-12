05aab99ad24b95b5586f2503cde028bf9da30a55
Carter Slocum <carter440@gmail.com>
2017-02-11 18:38:16 -0800

    Listen to port 80

--- TestServer.py
+++ TestServer.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_page():
 	return render_template('CarterSlocum.html')
@@ -18,4 +19,4 @@ def work_page():
 	return render_template('CarterProjects.html')
 
 if __name__ == "__main__":
-	app.run()	
+	app.run(host='0.0.0.0', port=80)	

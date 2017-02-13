e8e98a39155d006b840de62017b32d6006fc8e33
Carter Slocum <carter440@gmail.com>
2017-02-12 21:22:40 -0800

    first blog post!

--- TestServer.py
+++ TestServer.py
@@ -13,11 +13,15 @@ def about_page():
 
 @app.route('/blog')
 def blog_page():
-	return render_template('CarterBlog.html')
+	return redirect(url_for('blog_post', post='FirstPost'))
+
+@app.route('/blog/<post>', methods=['GET'])
+def blog_post(post):
+    return render_template("BlogPosts/" + post + ".html")
 
 @app.route('/projects')
 def work_page():
 	return render_template('CarterProjects.html', posts = projects.makePosts("ProjectPosts.txt"))
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80)	
+	app.run(host='0.0.0.0',port=80)	

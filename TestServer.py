a4f30e3914dc9be934f59ac77924b97e484ac535
Carter Slocum <carter440@gmail.com>
2017-02-15 20:43:39 -0800

    CSS fixes

--- TestServer.py
+++ TestServer.py
@@ -24,4 +24,4 @@ def work_page():
 	return render_template('CarterProjects.html', posts = projects.makePosts("ProjectPosts.txt"))
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0',port=80)	
+	app.run()	
